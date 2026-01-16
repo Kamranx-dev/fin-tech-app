f2a74b35e4e4a7e1281708a0f123c5203219967b
Kamranx-dev <safarovkamran55@gmail.com>
2026-01-16 05:26:06 +0400

    first commit

---
+++ src/test/java/com/safarov/tech_app/TechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safarov.tech_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
